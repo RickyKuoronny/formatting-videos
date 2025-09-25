568165a5a0383ddaf1990b371837e60449be6da9
Ricky Kuoronny <n10666630@qut.edu.au>
2025-09-25 14:55:10 +1000

    5 iteration (s3)

--- backend/s3.js
+++ backend/s3.js
@@ -1,13 +1,29 @@
 // s3.js
 require('dotenv').config();
-const { S3Client, PutObjectCommand, GetObjectCommand } = require("@aws-sdk/client-s3");
+const { S3Client, PutObjectCommand, GetObjectCommand, CreateBucketCommand, HeadBucketCommand } = require("@aws-sdk/client-s3");
 const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
 const fs = require('fs');
 
 const s3Client = new S3Client({ region: 'ap-southeast-2' });
 const bucketName = 'n10666630-video-processor';
 
+
+async function ensureBucket() {
+    try {
+        await s3Client.send(new HeadBucketCommand({ Bucket: bucketName }));
+        console.log('Bucket exists:', bucketName);
+    } catch (err) {
+        if (err.name === 'NotFound') {
+            await s3Client.send(new CreateBucketCommand({ Bucket: bucketName }));
+            console.log('Bucket created:', bucketName);
+        } else {
+            throw err;
+        }
+    }
+}
+
 async function uploadFile(key, filePath, contentType) {
+    await ensureBucket(); // Ensure bucket exists before uploading
     const fileStream = fs.createReadStream(filePath);
     await s3Client.send(new PutObjectCommand({
         Bucket: bucketName,
@@ -19,8 +35,7 @@ async function uploadFile(key, filePath, contentType) {
 }
 
 async function getPresignedUrl(key) {
-    const command = new GetObjectCommand({ Bucket: bucketName, Key: key });
-    return await getSignedUrl(s3Client, command, { expiresIn: 3600 });
+    return await getSignedUrl(s3Client, new GetObjectCommand({ Bucket: bucketName, Key: key }), { expiresIn: 3600 });
 }
 
 module.exports = { uploadFile, getPresignedUrl };
